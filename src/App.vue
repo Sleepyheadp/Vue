b996fc7370167c3d1b0048b980b23931d2b456e3
17863965581 <m178639655181_2@163.com>
2020-02-11 12:31:58 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
   <div id="app">
     hello vue-douban
     我又写了一些东西
+    <div class="hh">
+      
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-
+    .hh{
+      width:100px;
+      height:100px;
+      background-color:pink;
+    }
 </style>
 
 <script>
