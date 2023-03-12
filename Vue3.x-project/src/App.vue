160748865e646daf5b706712f458108ab884444b
sleepyheadp <17863965581@163.com>
2023-03-12 11:09:03 +0800

    Vue2.x的props传参使用

--- Vue3.x-project/src/App.vue
+++ Vue3.x-project/src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" version=2 />
 </template>
 
 <style scoped></style>
--- Vue3.x-project/src/components/HelloWorld.vue
+++ Vue3.x-project/src/components/HelloWorld.vue
@@ -1,23 +1,41 @@
 <script>
+// Vue3.x的写法
 // <script setup>
-import { ref } from 'vue'
-
 // defineProps({
 //   msg: String,
-//   default:'Vite + Vue3.x'
+//   default: 'Vite + Vue3.x'
+// }, {
+//   version: Number,
+//   default: 3,
+//   validator(val) {
+//     return val > 2
+//   }
 // })
 
 export default {
   props: {
-    msg: String,
-    default: 'Vite + Vue2.x'
+    msg: {
+      type: String,
+      default: 'Vite + Vue2.x'
+    },
+    version: {
+      // 传参类型
+      type: Number,
+      // 参数默认值
+      default: 2,
+      // 添加传参时的验证条件。当val == 2时接收参数
+      // 一般不在传参时做逻辑处理吧？
+      validator(val) {
+        return val == 2
+      }
+    }
   }
 }
 
 </script>
 
 <template>
-  <p>hello {{ msg }}</p>
+  <p>hello {{ msg }} {{ version }}</p>
 </template>
 
 <style scoped></style>
