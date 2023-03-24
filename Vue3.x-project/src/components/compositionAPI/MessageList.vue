00dd8c337b7bf26ae4c98058046cefe2765b87a3
sleepyheadp <17863965581@163.com>
2023-03-24 22:21:16 +0800

    2023-03-24：watch监听ref响应性数据对象变化时的新值旧值获取

--- Vue3.x-project/src/components/compositionAPI/MessageList.vue
+++ Vue3.x-project/src/components/compositionAPI/MessageList.vue
@@ -3,8 +3,9 @@
         <h2>标题：{{ options.title }}</h2>
         <div>用户:{{ options.user.name }}</div>
         <div>活跃：{{ options.user.active ? "是" : "否" }}</div>
-        <div>年龄：{{ refInfo.age }}</div>
-        <div>性别：{{ refInfo.sex }}</div>
+        <div>年龄：{{ refInfo.user.age }}</div>
+        <div>性别：{{ refInfo.user.sex }}</div>
+        <div>个人信息：{{ refInfo.info }}</div>
         <input type="text" v-model="searchTerm">
         <ul>
             <li v-for="msg in searchedMessages" :key="msg.id">{{ msg.content }}</li>
@@ -12,8 +13,9 @@
         <button @click="messages = []">删除全部</button>
         <button @click="options.title = '我是标题'">修改标题</button>
         <button @click="options.user.name = '李四'">修改用户</button>
-        <button @click="refInfo.age = 18">修改年龄</button>
-        <button @click="refInfo.sex = '女'">修改性别</button>
+        <button @click="refInfo.info = '修改了个人信息...'">修改个人信息</button>
+        <button @click="refInfo.user.age = 18">修改年龄</button>
+        <button @click="refInfo.user.sex = '女'">修改性别</button>
     </div>
 </template>
 <script>
@@ -34,8 +36,11 @@ export default {
             }
         })
         const refInfo = ref({
-            age: 25,
-            sex: '男'
+            info: 'info...',
+            user: {
+                age: 25,
+                sex: '男'
+            }
         })
         // 在setup中访问需要通过value属性，模版语法中则直接取值就可以。
         console.log('ref.value:', messages.value)
@@ -43,7 +48,7 @@ export default {
         // computed计算属性
         const searchTerm = ref('')
         const searchedMessages = computed(() => {
-            if (searchTerm.value === '') return ''
+            if (searchTerm.value === '') return messages.value
             return messages.value.filter((msg) => {
                 return msg.content.includes(searchTerm.value)
             })
@@ -61,11 +66,12 @@ export default {
             console.log('options.title:', newVal, oldVal);
         })
         watch(
-            () => refInfo.value,
+            // 改为深拷贝的形式，可以监听到ref响应性数据中对象属性的变化。
+            // 也可以使用第三方插件
+            () => JSON.parse(JSON.stringify(refInfo.value)),
             (newVal, oldVal) => {
-                console.log('refInfo.value.age', newVal, oldVal, newVal === oldVal);
+                console.log('refInfo.value.user', newVal, oldVal, newVal === oldVal);
             },
-            { deep: true }
         )
         // watch(() => refInfo.value, (newVal, oldVal) => {
         //     console.log('refInfo.value.sex', newVal, oldVal);
