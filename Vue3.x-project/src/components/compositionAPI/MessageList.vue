0f18f12f7296f94f2d7ae04bd6cdb918619be6cb
sleepyheadp <17863965581@163.com>
2023-03-24 15:22:17 +0800

    2023-03-24：reactive的用法以及与ref的区别

--- Vue3.x-project/src/components/compositionAPI/MessageList.vue
+++ Vue3.x-project/src/components/compositionAPI/MessageList.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
+        <h2>标题：{{ options.title }}</h2>
+        <P>用户:{{ options.user.name }}，活跃：{{ options.user.active ? "是" : "否" }}</P>
         <ul>
             <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
         </ul>
         <button @click="messages = []">删除全部</button>
+        <button @click="options.title = '我是标题'">修改标题</button>
+        <button @click="options.user.name = '李四'">修改用户</button>
     </div>
 </template>
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 export default {
     setup() {
         const messages = ref([
@@ -16,9 +20,17 @@ export default {
             { id: 3, content: "这是一条消息提醒3" },
             { id: 4, content: "这是一条消息提醒4" },
         ]);
+        const options = reactive({
+            title: '消息列表',
+            user: {
+                name: '张三',
+                active: true
+            }
+        })
         // 在setup中访问需要通过value属性，模版语法中则直接取值就可以。
-        console.log(messages.value)
-        return { messages };
+        console.log('ref.value:', messages.value)
+        console.log('reactive', options.title);
+        return { messages, options };
     },
 };
 </script>
