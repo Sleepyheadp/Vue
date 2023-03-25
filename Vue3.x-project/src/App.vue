702fbd151a8d60c53de45272d23ef6b93c0d0627
sleepyheadp <17863965581@163.com>
2023-03-25 21:39:59 +0800

    2023-03-25：methods(setup

--- Vue3.x-project/src/App.vue
+++ Vue3.x-project/src/App.vue
@@ -1,17 +1,17 @@
-<script>
-import Home from "./page/home.vue"
-import CompositionApiMain from './page/CompositionApiMain.vue'
-export default {
-  components: { Home, CompositionApiMain }
-}
-</script>
-
 <template>
   <!-- vue2.x要求template模版中必须有一个根元素 -->
   <div id="root">
     <!-- <Home></Home> -->
-    <CompositionApiMain></CompositionApiMain>
+    <!-- <CompositionApiMain></CompositionApiMain> -->
+    <List></List>
   </div>
 </template>
-
+<script>
+import Home from "./page/home.vue"
+import CompositionApiMain from './page/CompositionApiMain.vue'
+import List from './page/list.vue'
+export default {
+  components: { Home, CompositionApiMain, List },
+}
+</script>
 <style scoped></style>
---
+++ Vue3.x-project/src/page/list.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <MessageList :messages="messages"></MessageList>
+    </div>
+</template>
+<script>
+import MessageList from '../components/messages/MessageList.vue'
+import { ref } from 'vue'
+export default {
+    components: { MessageList },
+    setup() {
+        const messages = ref([
+            { id: 1, content: "这是一条消息提醒1" },
+            { id: 2, content: "这是一条消息提醒2" },
+            { id: 3, content: "这是一条消息提醒3" },
+            { id: 4, content: "这是一条消息提醒4" },
+        ]);
+        return { messages }
+    }
+}
+</script>
+<style scoped></style>
