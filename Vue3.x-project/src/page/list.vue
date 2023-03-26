c43a0d96666dcec07a330e117d7f32eb4b4e2d33
sleepyheadp <17863965581@163.com>
2023-03-26 11:43:26 +0800

    2023-03-26：渲染模版-slots

--- Vue3.x-project/src/page/list.vue
+++ Vue3.x-project/src/page/list.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
         <MessageList></MessageList>
+        <BaseLayout>
+            <template v-slot:header>
+                <nav>
+                    <a href="#">首页</a>
+                    <a href="#">关于</a>
+                    <a href="#">联系</a>
+                </nav>
+            </template>
+            <template v-slot:default>
+                <p>这是内容</p>
+            </template>
+        </BaseLayout>
     </div>
 </template>
 <script>
 import MessageList from '../components/messages/MessageList.vue'
-import { ref } from 'vue'
+import BaseLayout from '../components/BaseLayout.vue'
 export default {
-    components: { MessageList },
+    components: { MessageList, BaseLayout },
     setup() {
     }
 }
