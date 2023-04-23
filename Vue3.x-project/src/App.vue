c7232e7f4fd6648a7817143f6f0f73b731f0d9bc
sleepyheadp <17863965581@163.com>
2023-04-24 00:11:53 +0800

    2023-04-24：不用vue-router实现页面的切换

--- Vue3.x-project/src/App.vue
+++ Vue3.x-project/src/App.vue
@@ -4,14 +4,16 @@
     <!-- <Home></Home> -->
     <!-- <CompositionApiMain></CompositionApiMain> -->
     <!-- <List></List> -->
-    <TransitionComp></TransitionComp>
+    <!-- <TransitionComp></TransitionComp> -->
+    <VueRouter></VueRouter>
   </div>
 </template>
 <script setup>
+import { Transition } from 'vue'
 import Home from "./page/home.vue"
 import CompositionApiMain from './page/CompositionApiMain.vue'
 import List from './page/list.vue'
 import TransitionComp from './page/TransitionComp.vue'
-import { Transition } from 'vue'
+import VueRouter from './page/vueRouter.vue'
 </script>
 <style scoped></style>
