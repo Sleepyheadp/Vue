0bafc76fab12a60547a955d4a5a7a02cf4ad4dcf
sleepyheadp <17863965581@163.com>
2023-04-23 13:43:18 +0800

    2023-04-23：component:is vue组件切换动画

--- Vue3.x-project/src/page/TransitionComp.vue
+++ Vue3.x-project/src/page/TransitionComp.vue
@@ -4,6 +4,7 @@
         <!-- <AnimationInOut></AnimationInOut> -->
         <!-- <TransitionInOut></TransitionInOut> -->
         <TransitionComps></TransitionComps>
+        <TransitionCompsMultiple></TransitionCompsMultiple>
     </div>
 </template>
 <script setup>
@@ -11,5 +12,7 @@ import AnimationInOut from '../components/Transition-comp/Animation-in-out.vue';
 import TransitionComps from '../components/Transition-comp/Transition-comps.vue';
 import TransitionInOut from '../components/Transition-comp/Transition-in-out.vue';
 import vbindAnimation from '../components/Transition-comp/vbindAnimation.vue';
+// 多个vue组件切换动画 animation
+import TransitionCompsMultiple from '../components/Transition-comp/Transition-comps-multiple.vue'
 </script>
 <style></style>
