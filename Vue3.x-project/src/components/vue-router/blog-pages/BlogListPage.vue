5b530797bf186187f77cd4523d1c2ec2b0632da0
sleepyheadp <17863965581@163.com>
2023-04-26 14:05:46 +0800

    2023-04-26：获取query查询参数

--- Vue3.x-project/src/components/vue-router/blog-pages/BlogListPage.vue
+++ Vue3.x-project/src/components/vue-router/blog-pages/BlogListPage.vue
@@ -4,6 +4,9 @@
             <router-link :to="`/${blogPost.id}`" class="title">{{ blogPost.title }}</router-link>
             <div class="body">{{ blogPost.body }}</div>
         </div>
+        <!-- 浏览器地址栏输入：/blogList/name=Capoo&age=18 -->
+        <p>query:{{ $route.query }}</p>
+        <!-- query:{ "name": "Capoo", "age": "18" } -->
     </div>
 </template>
 
