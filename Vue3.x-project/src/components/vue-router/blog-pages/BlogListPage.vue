c2d7d17096118f4359bffe7e5f45e7be1ea11ef8
sleepyheadp <17863965581@163.com>
2023-04-26 23:47:20 +0800

    2023-04-26：编程式控制导航跳转 $router.push/replace/go

--- Vue3.x-project/src/components/vue-router/blog-pages/BlogListPage.vue
+++ Vue3.x-project/src/components/vue-router/blog-pages/BlogListPage.vue
@@ -3,13 +3,26 @@
         <div v-for="blogPost in blogPosts" :key="blogPost.id">
             <router-link :to="`/${blogPost.id}`" class="title">{{ blogPost.title }}</router-link>
             <div class="body">{{ blogPost.body }}</div>
+            <!-- 通过字符串跳转 -->
+            <!-- <button @click="$router.push(`/${blogPost.id}`)">查看全文</button> -->
+            <!-- 使用配置对象跳转 -->
+            <!-- <button @click="$router.push({
+                name: 'blogPost',
+                params: { postId: blogPost.id }
+            })">查看全文</button> -->
+            <!-- 通过$router.replace方法实现跳转：会替代当前路径为根路径 -->
+            <!-- <button @click="$router.replace({
+                name: 'blogPost',
+                params: { postId: blogPost.id }
+            })">查看全文</button> -->
+            <!-- 推回到上上个路径 -->
+            <button @click="$router.go(-2)">查看全文</button>
         </div>
         <!-- 浏览器地址栏输入：/blogList/name=Capoo&age=18 -->
         <p>query:{{ $route.query }}</p>
         <!-- query:{ "name": "Capoo", "age": "18" } -->
     </div>
 </template>
-
 <script>
 import { getAllPosts } from "../../../data/blogPosts"
 export default {
