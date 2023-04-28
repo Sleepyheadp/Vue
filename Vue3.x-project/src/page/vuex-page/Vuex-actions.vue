d04631b033b7672f0a1c1677d5448bb0ddfee882
sleepyheadp <17863965581@163.com>
2023-04-28 21:19:19 +0800

    2023-04-28：actions

---
+++ Vue3.x-project/src/page/vuex-page/Vuex-actions.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-if="loading">loading...</div>
+        <div v-else>
+            <p v-for="user in users" :key="user.id">
+                user: <span>{{ user }}</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex"
+export default {
+    created() {
+        // this.$store.dispatch("fetchUsers")
+        // this.fetchUsers()
+        this.fetchUsers({ limit: 2 })
+    },
+    computed: mapState(["users", 'loading']),
+    methods: mapActions(['fetchUsers'])
+}
+</script>
+<style scoped></style>
