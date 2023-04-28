01a19c6155d6e762272c6b4d84dc998e01efdb3f
sleepyheadp <17863965581@163.com>
2023-04-28 16:40:26 +0800

    2023-04-28：change fileName

--- Vue3.x-project/src/page/vuex-page/index.vue
+++ Vue3.x-project/src/page/vuex-page/index.vue
@@ -2,12 +2,12 @@
     <div>
         <!-- vuex案例：随机切换颜色 -->
         <!-- <ChangeColorFirstDemo></ChangeColorFirstDemo> -->
-        <MapState></MapState>
+        <VuexDemo></VuexDemo>
     </div>
 </template>
 
 <script setup>
 import ChangeColorFirstDemo from "./changeColor-first-demo.vue"
-import MapState from "./mapState.vue"
+import VuexDemo from "./Vuex-demo.vue"
 </script>
 <style scoped></style>
