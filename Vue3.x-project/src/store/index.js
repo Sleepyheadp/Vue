9cb73487a90d3fe4be4d5e541e2362041b8cf95a
sleepyheadp <17863965581@163.com>
2023-04-30 08:50:41 +0800

    2023-04-30：配置日志插件参数createLogger({})

--- Vue3.x-project/src/store/index.js
+++ Vue3.x-project/src/store/index.js
@@ -5,7 +5,37 @@ import mutations from "./mutations";
 import { usersModules } from "./modules/users-modules";
 import { blogsModules } from "./modules/blogs-modules";
 export const store = createStore({
-	plugins: [createLogger()],
+	plugins: [
+		createLogger({
+			collapsed: false, // 默认展开
+			// logActions: false,
+			// logMutations: false,
+			filter(mutation, stateBefore, stateAfter) {
+				// stateBefore 或 stateAfter 可以用于过滤符合某些 state 变化的 mutations
+				return mutation.type === "addUser";
+			},
+			actionFilter(action, state) {
+				return action.type === "addUserAsync";
+			},
+			transformer(state) {
+				// 只关注 Users 下的 list state 的变化
+				return state.users.list;
+			},
+			mutationTransformer(mutation) {
+				return `触发了 ${mutation.type}，payload 为 ${JSON.stringify(
+					mutation.payload
+				)}`;
+			},
+			actionTransformer(action) {
+				return `触发了 ${action.type}，payload 为 ${JSON.stringify(
+					action.payload
+				)}`;
+			},
+			logger: {
+				log: (log, styles, details) => console.error(log, styles, details), // details，例如触发的 action、mutation 等信息，和状态的变化
+			},
+		}),
+	],
 	modules: {
 		usersModules,
 		blogsModules,
