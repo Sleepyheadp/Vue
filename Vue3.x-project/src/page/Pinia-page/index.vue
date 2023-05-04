b5db0aaf511fd4f74f14434896086177d36475f9
sleepyheadp <17863965581@163.com>
2023-05-04 16:32:38 +0800

    2023-05-04：添加状态并在组件中使用

---
+++ Vue3.x-project/src/page/Pinia-page/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>我的笔记本</h1>
+        <SearchNote></SearchNote>
+        <NoteList></NoteList>
+        <AddNote></AddNote>
+    </div>
+</template>
+
+<script setup>
+import AddNote from "../../components/piniaPages/AddNote.vue"
+import NoteList from "../../components/piniaPages/NoteList.vue"
+import SearchNote from "../../components/piniaPages/SearchNote.vue"
+</script>
+<style scoped></style>
