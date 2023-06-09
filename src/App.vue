26eb218637401c4203ba25f346474378b4cb47ec
sleepyheadp <17863965581@163.com>
2023-06-09 22:38:54 +0800

    2023-06-09：create project | pages | router

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header class="header">
+      <NavBar />
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import './assets/base.css'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<style scoped>
+.header {
+  height: 80px;
+  box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.08);
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>LoginPage</div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped></style>
