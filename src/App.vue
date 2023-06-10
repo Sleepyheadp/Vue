290d05d70a9c9b9b5924c7eb3af80d9f5e135190
sleepyheadp <17863965581@163.com>
2023-06-10 22:11:02 +0800

    2023-06-10：底部标识添加（Capoo. All Rights Reserved.

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@
             <router-view></router-view>
         </div>
     </template>
+    <footer class="footer" :class="{ inside: $route.name === 'login' }">
+        &copy;Capoo. All Rights Reserved.
+    </footer>
 </template>
 <script setup>
 import './assets/base.css'
@@ -21,4 +24,21 @@ import NavBar from './components/NavBar.vue'
 import TheLayout from './components/TheLayout.vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+    height: 80px;
+    box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.08);
+}
+
+.footer {
+    text-align: center;
+    padding: 38px 0;
+    color: #828282;
+}
+
+.footer.inside {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+</style>
