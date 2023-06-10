3d45e710a619658bf838359eb587aa291688a584
sleepyheadp <17863965581@163.com>
2023-06-10 15:37:00 +0800

    2023-06-10：封装弹窗组件的架子（Teleport组件传送

---
+++ src/components/TheModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <!-- 弹窗的框架，内容通过slot自定义 -->
+    <Teleport to="body">
+        <div class="modal">
+            <div class="backdrop"></div>
+            <div class="modalContent">
+                <button class="closeBtn" v-if="popupShow" @click="props.popupShow = false">
+                    <TheIcon icon="close" />
+                </button>
+                <slot></slot>
+            </div>
+        </div>
+    </Teleport>
+</template>
+<script setup>
+import TheIcon from "./TheIcon.vue";
+</script>
+<style scoped>
+.modal {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    max-width: 100%;
+    left: 0;
+    top: 0;
+    display: grid;
+    place-items: center;
+}
+
+.backdrop {
+    background: rgba(0, 0, 0, 0.56);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+}
+
+.modalContent {
+    position: relative;
+    background: white;
+    border-radius: 52px;
+    overflow: hidden;
+}
+
+.closeBtn {
+    position: absolute;
+    background: none;
+    border: none;
+    right: 14px;
+    top: 10px;
+}
+
+.closeBtn svg {
+    width: 54px;
+    height: 54px;
+}
+</style>
