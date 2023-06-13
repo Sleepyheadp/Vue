e6d7a87607aa1ee6e2530b3c32e2c16bcb7018a4
sleepyheadp <17863965581@163.com>
2023-06-13 12:04:25 +0800

    2023-06-13：三种ref类型定义写法

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+
+import { ref, type Ref } from 'vue'
+
+// 1、ref 自动类型推断
+const num = ref(0)
+// 2、使用Ref类型
+const str: Ref<string> = ref('str')
+// 3、使用泛型
+const bool = ref<boolean>(true)
+
+console.log(num.value, str.value, bool.value);
+
 </script>
 
 <template>
